9b0d8db1ee0a34047bbcee0c8317932c11f19136
akash_pawara_ <akashpawara29@gmail.com>
2020-08-18 22:57:56 +0530

    testing

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -4,6 +4,20 @@ $( "#login-auth" ).click(function() {
     $("#vendor-login-page").css("display", "none");
     $("#vendor-dashboard").css("display", "block");
 });
+$( "#register-auth" ).click(function() {
+    $("#login-foot").css("display", "none");
+    $("#vendor-login-page").css("display", "none");
+    $("#vendor-shop-registration").css("display", "block");
+});
+$( "#next-shop-form" ).click(function() {
+    $("#vendor-shop-registration-owner-form").css("display", "none");
+    $("#vendor-shop-registration-shop-form").css("display", "block");
+});
+$( "#next-dashboard" ).click(function() {
+    $("#vendor-shop-registration-shop-form").css("display", "none");
+    $("#foot").css("display", "flex");
+    $("#vendor-dashboard").css("display", "block");
+});
 $( "#logout-auth" ).click(function() {
     $("#login-foot").css("display", "flex");
     $("#foot").css("display", "none");
